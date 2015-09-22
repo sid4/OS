0fbb409ac081a2cf5c75fc8518dddf5dba0da7dd
Prateek Bhat <pratbhat@iu.edu>
2015-09-21 20:51:15 -0400

    uploaded prodcons.h

---
+++ bbb-xinu/include/prodcons.h
@@ -0,0 +1,11 @@
+/* prodcons.h */
+
+#include <stddef.h>
+#include <stdio.h>
+
+/* Global variable for Producer Consumer */
+extern int n; /* Decleration of global variable n */
+
+/* function prototype */
+void consumer (int count);
+void producer (int count);
